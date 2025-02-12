e62d85c88e664520bf2aff16eba2dee6f7fe76c6
chenqiliang <qiliang.chen@zhongcui.com>
2025-02-12 18:24:02 +0800

    test method

---
+++ tenum/tenum.go
@@ -0,0 +1,26 @@
+package tenum
+
+import (
+	"fmt"
+)
+
+// 定义一个常量组，用于表示枚举
+type Fruit int
+
+const (
+	Apple  Fruit = iota // 0
+	Banana              // 1
+	Cherry              // 2
+)
+
+// String 方法用于实现 fmt.Stringer 接口，便于打印枚举值
+func (f Fruit) String() string {
+	return [...]string{"Apple", "Banana", "Cherry"}[f]
+}
+
+func main() {
+	var myFruit Fruit = Banana
+
+	fmt.Println("My favorite fruit is:", myFruit)
+	fmt.Println("Fruit value as integer:", int(myFruit), Fruit(2))
+}
---
+++ tmethod/tmethod.go
@@ -0,0 +1,20 @@
+package tmethod
+
+import "fmt"
+
+type Slice []int
+
+func (s Slice) Set(i int, v int) {
+	s[i] = v
+}
+
+func (s Slice) Append(val int) {
+	s = append(s, val)
+}
+
+func main() {
+	s := make(Slice, 2)
+	s.Set(0, 1)
+	s.Append(2)
+	fmt.Println(s)
+}
